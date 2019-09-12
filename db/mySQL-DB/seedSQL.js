ac6bd2654dc717813d1db758976353445c2f482c
tjlee527 <tjlee527@gmail.com>
2019-09-12 14:57:38 -0700

    able to bulk seed using mysql for questions

--- db/mySQL-DB/seedSQL.js
+++ db/mySQL-DB/seedSQL.js
@@ -2,103 +2,73 @@ const mysql = require('mysql');
 const faker = require('faker');
 const db = require('./indexSQL.js');
 
-// // INSERTING DATA FOR SIZING TABLE
-// const sizingArrayToSeed = [
-//   { size: 'XS', neck: '13-13.5', chest: '30-32', sleeve: '31.5-32' },
-//   { size: 'S', neck: '14-14.5', chest: '34-36', sleeve: '32.5-33' },
-//   { size: 'M', neck: '15-15.5', chest: '38-40', sleeve: '33.5-34' },
-//   { size: 'L', neck: '16-16.5', chest: '42-44', sleeve: '34.5-35' },
-//   { size: 'XL', neck: '17-17.5', chest: '46-48', sleeve: '35.5-36' },
-//   { size: 'XXL', neck: '18-18.5', chest: '50-52', sleeve: '36.5-37' },
-//   { size: 'XXXL', neck: '19-19.5', chest: '54-56', sleeve: '37.5-38' }
-// ];
+// INSERTING DATA FOR SIZING TABLE
+const sizingArrayToSeed = [
+  { size: 'XS', neck: '13-13.5', chest: '30-32', sleeve: '31.5-32' },
+  { size: 'S', neck: '14-14.5', chest: '34-36', sleeve: '32.5-33' },
+  { size: 'M', neck: '15-15.5', chest: '38-40', sleeve: '33.5-34' },
+  { size: 'L', neck: '16-16.5', chest: '42-44', sleeve: '34.5-35' },
+  { size: 'XL', neck: '17-17.5', chest: '46-48', sleeve: '35.5-36' },
+  { size: 'XXL', neck: '18-18.5', chest: '50-52', sleeve: '36.5-37' },
+  { size: 'XXXL', neck: '19-19.5', chest: '54-56', sleeve: '37.5-38' }
+];
 
-// const masterArr = [];
-// sizingArrayToSeed.forEach((seed) => {
-//   const infoArr = ['Men - Shirts', seed.size, seed.neck, seed.chest, seed.sleeve];
-//   masterArr.push(infoArr);
-// });
+const masterArr = [];
+sizingArrayToSeed.forEach((seed) => {
+  const infoArr = ['Men - Shirts', seed.size, seed.neck, seed.chest, seed.sleeve];
+  masterArr.push(infoArr);
+});
 
-// const sizingQuery = `INSERT INTO sizing
-//     (type, size, neck, chest, sleeve)
-//     VALUES ?`;
-//   db.query(sizingQuery, [masterArr], (err, res) => {
-//     if (err) throw err;
-//     if (res) console.log('seeded sizing data');
-//   });
+const sizingQuery = `INSERT INTO sizing
+    (type, size, neck, chest, sleeve)
+    VALUES ?`;
+  db.query(sizingQuery, [masterArr], (err, res) => {
+    if (err) throw err;
+    if (res) console.log('seeded sizing data');
+  });
 
-// // INSERTING DATA FOR QUESTIONS
-// const getRandomItemId = () => faker.random.number({ min: 1, max: 100 });
-// const checkIfQuestionHasAnswer = function () {
-//   const hasAnswer = faker.random.number({ min: 0, max: 1 });
-//   const answerArray = [];
-//   if (hasAnswer) {
-//     answerArray.push(faker.lorem.sentence(), faker.name.firstName(), faker.date.past());
-//     answerArray.push(faker.random.number({ min: 0, max: 5 }), faker.random.number({ min: 0, max: 5 }), faker.random.boolean());
-//   } else {
-//     answerArray.push(null, null, null, null, null, null);
-//   }
-//   return answerArray;
-// };
+// INSERTING DATA FOR QUESTIONS
+const getRandomItemId = () => faker.random.number({ min: 1, max: 100 });
+const checkIfQuestionHasAnswer = function () {
+  const hasAnswer = faker.random.number({ min: 0, max: 1 });
+  const answerArray = [];
+  if (hasAnswer) {
+    answerArray.push(faker.lorem.sentence(), faker.name.firstName(), faker.date.past());
+    answerArray.push(faker.random.number({ min: 0, max: 5 }), faker.random.number({ min: 0, max: 5 }), faker.random.boolean());
+  } else {
+    answerArray.push(null, null, null, null, null, null);
+  }
+  return answerArray;
+};
 
-// // generate 200 (will have to adjust this num later) random questions for random itemIds
-// const allQs = [];
-// let questionsAdded = 1;
-// for (let index = 1; index <= 2; index += 1) {
-//   const getAnswers = checkIfQuestionHasAnswer();
-//   const qObj = {
-//     itemId: getRandomItemId(),
-//     question: faker.lorem.sentence(),
-//     asker: faker.name.firstName(),
-//     dateAsked: faker.date.past(),
-//     answer: getAnswers[0],
-//     nameOfResponder: getAnswers[1],
-//     dateAnswered: getAnswers[2],
-//     helpfulCount: getAnswers[3],
-//     unhelpfulCount: getAnswers[4],
-//     teamMember: getAnswers[5],
-//   };
-//   // const questArr = Object.keys(qObj).map((key) => {
-//   //   return qObj[key];
-//   // });
-//   const questArr = [qObj.itemId, qObj.question, qObj.asker, qObj.dateAsked, qObj.answer, qObj.nameOfResponder, qObj.dateAnswered, qObj.helpfulCount, qObj.unhelpfulCount, qObj.teamMember];
+// generate 200 (will have to adjust this num later) random questions for random itemIds
+const allQs = [];
+let questionsAdded = 1;
+for (let index = 1; index <= 2; index += 1) {
+  const getAnswers = checkIfQuestionHasAnswer();
+  const qObj = {
+    itemId: getRandomItemId(),
+    question: faker.lorem.sentence(),
+    asker: faker.name.firstName(),
+    dateAsked: faker.date.past(),
+    answer: getAnswers[0],
+    nameOfResponder: getAnswers[1],
+    dateAnswered: getAnswers[2],
+    helpfulCount: getAnswers[3],
+    unhelpfulCount: getAnswers[4],
+    teamMember: getAnswers[5],
+  };
+  const questArr = Object.keys(qObj).map((key) => {
+    return qObj[key];
+  });
+  allQs.push(questArr);
+}
 
-//   allQs.push(questArr);
-//   // console.log('index',index, alQs);
-//   // const questionQuery = 'INSERT INTO questions (itemId, question, asker, dateAsked, answer, nameOfResponder, dateAnswered, helpfulCount, unhelpfulCount, targetTeamMember) VALUES (?)';
-//   // db.query(questionQuery, [questArr], (err, res) => {
-//   //   if (err) throw err;
-//   //   if (res) console.log('seeded questions data rows:' + questionsAdded++);
-//   // });
-// }
-
-// // var test = [ [ 64,
-// //   'Eum sint beatae vero.',
-// //   'Randall',
-// //   faker.date.past(),
-// //   'Nam quas architecto cum quibusdam exercitationem.',
-// //   'Keenan',
-// //   faker.date.past(),
-// //   5,
-// //   5,
-// //   false ],
-// //   [ 64,
-// //     'Eum sint beatae vero.',
-// //     'Randall',
-// //     faker.date.past(),
-// //     'Nam quas architecto cum quibusdam exercitationem.',
-// //     'Keenan',
-// //     faker.date.past(),
-// //     5,
-// //     5,
-// //     false ]]
-// console.log(allQs);
-
-// const questionQuery = 'INSERT INTO questions (itemId, question, asker, dateAsked, answer, nameOfResponder, dateAnswered, helpfulCount, unhelpfulCount, targetTeamMember) VALUES ?';
-// db.query(questionQuery, allQs, (err, res) => {
-//   if (err) throw err;
-//   if (res) console.log('seeded questions data rows:' + res.affectedRows);
-// });
+const questionQuery = 'INSERT INTO questions (itemId, question, asker, dateAsked, answer, nameOfResponder, dateAnswered, helpfulCount, unhelpfulCount, targetTeamMember) VALUES ?';
+db.query(questionQuery, [allQs], (err, res) => {
+  if (err) throw err;
+  if (res) console.log('seeded questions data rows:' + res.affectedRows);
+});
 
 // INSERTING DATA FOR ITEM-DETAIL
 // generate random bullet ponts for item-detail
@@ -155,7 +125,6 @@ for (let j = 1; j <= 10; j += 1) {
     return itemObj[key];
   });
   allDetail.push(detailArr);
-  // add into db table for each item
 }
 
 const detailQuery = `INSERT INTO detail
