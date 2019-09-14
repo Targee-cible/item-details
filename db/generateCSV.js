5ad51e7581d91d1cdada8f60e54a947a186a8088
tjlee527 <tjlee527@gmail.com>
2019-09-13 17:09:15 -0700

    added data generation

--- db/generateCSV.js
+++ db/generateCSV.js
@@ -76,7 +76,7 @@ const fs = require('fs');
 // } 
 
 // // loop to create multiple files
-// for (var i = 0; i < 3; i++) {
+// for (var i = 1; i <= 3; i++) {
 //   const quesCSV = convertQuesToCSV(generateQuestionJSON());
 //   fs.writeFileSync(`./db/CSVdata/questionData${i}.csv`, quesCSV);
 // }
@@ -101,7 +101,7 @@ const randomBulletPoints = function () {
   // loop to add into item detail table, will have to adjust the 100 num
 const generateDetailJSON = function () {
   let allDetail = [];
-  for (let j = 1; j <= 3; j += 1) { // adjust for amount of data wanted
+  for (let j = 1; j <= 50000; j += 1) { // adjust for amount of data wanted
     const pointsToList = randomBulletPoints();
     const itemObj = {
       id: 'null',
@@ -146,7 +146,7 @@ const convertDetailToCSV = function( JSONobj ) {
   return str.join('\n');
 } 
 
-for (var i = 0; i < 3; i++) {
+for (var i = 1; i <= 10; i++) {
   const detailCSV = convertDetailToCSV(generateDetailJSON());
   fs.writeFileSync(`./db/CSVdata/detailData${i}.csv`, detailCSV);
 }
--- db/mySQL-DB/seedingCSV.js
+++ db/mySQL-DB/seedingCSV.js
@@ -3,7 +3,10 @@ const faker = require('faker');
 const db = require('./indexSQL.js');
 const genFunc = require('./generateFunc.js');
 
-let maxCount = 0; 
+const maxCount = 15; // reassign for when you want seeding to end
+let startCount = 0;
+const startTime = new Date();
+
 // // INSERTING DATA FOR SIZING TABLE
 // const seedSize = () => {
 //   const sizingQuery = `LOAD DATA LOCAL INFILE './db/CSVdata/sizingData.csv' 
@@ -15,35 +18,44 @@ let maxCount = 0;
 //     });
 //   }) 
 // };
-// seedSize()
-//   .then((rowsAdded) => {
-//     console.log('seeded sizing data rows:' + rowsAdded);
-//     db.end();
-//   })
 
-// INSERTING DATA FOR QUESTIONS
-const seedQuestions = (fileCount) => {
-  const path = `./db/CSVdata/questionData${fileCount}.csv`
-  const questionQuery = `LOAD DATA LOCAL INFILE ?
-  INTO TABLE questions FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'` ;
-  return new Promise((resolve, reject) => {
-    db.query(questionQuery, path, (err, res) => {
-      if (err) reject(err);
-      if (res) resolve(res.affectedRows); 
-    });
-  }) 
-}
 
-const recursiveSeedQuestions = (fileNum, currentTime, end) => {
-  seedQuestions(fileNum)
-    .then((rows) => {
-      console.log('added rows', rows);
-      if (currentTime !== end) {
-        recursiveSeedQuestions(fileNum + 1, currentTime + 1, end, table);
-      } 
-      console.log('done seeding table');
-    })
-}
+// // INSERTING DATA FOR QUESTIONS
+// const seedQuestions = (fileCount) => {
+//   const path = `./db/CSVdata/questionData${fileCount}.csv`
+//   const questionQuery = `LOAD DATA LOCAL INFILE ?
+//   INTO TABLE questions FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'` ;
+//   return new Promise((resolve, reject) => {
+//     db.query(questionQuery, path, (err, res) => {
+//       if (err) reject(err);
+//       if (res) resolve(res.affectedRows); 
+//     });
+//   }) 
+// }
+
+
+//   seedQuestions(fileNum)
+//     .then((rows) => {
+//       console.log('added rows', rows);
+//       if (currentTime !== end) {
+//         recursiveSeedQuestions(fileNum + 1, currentTime + 1, end);
+//       } 
+//       return Promise.resolve('done questions');
+//       console.log('done seeding table');
+//     })
+// }
+
+// const recursiveSeedQuestions = (fileNum, currentTime, end) => {
+//   seedQuestions(fileNum)
+//     .then((rows) => {
+//       console.log('added rows', rows);
+//       if (currentTime !== end) {
+//         recursiveSeedQuestions(fileNum + 1, currentTime + 1, end);
+//       } 
+//       return Promise.resolve('done questions');
+//       console.log('done seeding table');
+//     })
+// }
 
 
 // recursiveSeed(0, 0, 2);
@@ -51,6 +63,9 @@ const recursiveSeedQuestions = (fileNum, currentTime, end) => {
 
 // // INSERTING DATA FOR ITEM-DETAIL
 const seedDetail = (fileCount) => {
+  if (fileCount > 10) {
+    fileCount = fileCount.toString().split('').pop();
+  }
   const path = `./db/CSVdata/detailData${fileCount}.csv`
   const questionQuery = `LOAD DATA LOCAL INFILE ?
   INTO TABLE detail FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'` ;
@@ -65,43 +80,29 @@ const seedDetail = (fileCount) => {
 const recursiveSeedDetail = (fileNum, currentTime, end) => {
   seedDetail(fileNum)
     .then((rows) => {
-      maxCount++;
-      console.log('count', maxCount);
+      startCount++;
+      console.log('count', startCount);
       console.log('added rows', rows);
       if (currentTime !== end) {
         recursiveSeedDetail(fileNum + 1, currentTime + 1, end);
       } 
-      if (maxCount === 3) {
+      if (currentTime === end) {
         db.end();
+        console.log(new Date() - startTime);
       }
       console.log('done seeding detail');
     })
 }
 
-recursiveSeedDetail(0, 0, 2);
+recursiveSeedDetail(0, startCount, maxCount);
 
-// var insertBatchDetail = function() {
-//   for (var i = 0; i < 3; i++) {
-//     const allDetail = [];
-//     const generatedDetail = genFunc.createDetailBatch();
-//     generatedDetail.forEach((item) => {
-//       const detailArr = Object.keys(item).map((key) => item[key]);
-//       allDetail.push(detailArr)
-//     })
-//     const detailQuery = `INSERT INTO detail
-//           (itemId, fitAndSTylePointOne, fitAndSTylePointTwo,
-//           fitAndSTylePointThree, fitAndSTylePointFour, fitAndSTylePointFive, 
-//           fitAndSTyleBlurb, sizing, material, fit, length, features, neckline, 
-//           itemStyle, garmentCuffCutType, garmentSleeveStyle, careAndCleaning, 
-//           TCIN, UPC, DPCI, origin, recycledPolyester, fastShipping, estimatedShipDimensions, 
-//           estimatedShipWeight, type) values ?`;
-//     return new Promise( db.query(detailQuery, [allDetail], (err, res) => {
-//       if (err) throw err;
-//       if (res) console.log('seeded detail data rows:' + res.affectedRows);
-//     });
-//   }
-// };
-
-if (maxCount === 3) {
-  db.end();
-}
+// // chain the functions
+// seedSize()
+//   .then((rowsAdded) => {
+//     console.log('seeded sizing data rows:' + rowsAdded);
+//     recursiveSeedQuestions(0, 0, 2)
+//       .then((done) => {
+//         console.log(done);
+//       })  
+//   })
+  
