5763f1e0dad6e0c29c9da2841d26549ebd93b570
tjlee527 <tjlee527@gmail.com>
2019-09-14 13:39:22 -0700

    able to seed recursively for questions

--- db/couchDB/seedCouch.js
+++ db/couchDB/seedCouch.js
@@ -1,14 +1,36 @@
 const db = require('./indexCouch.js');
-const gen = require('./dataGenFunc.js');
+const gen = require('../generateCSV.js');
+
+// need 99 cyles of detail => maxCountDetail = 99
+const maxCountDetail = 99; // reassign for when you want seeding to end
+let startCountDetail = 0;
+// need 50 cycles of questions => maxCountQuestions = 49
+const maxCountQuestions = 9;
+let startCountQuestions = 0;
+let totalCountDetail = 0; 
+let totalCountQuestions = 0;
+const startTime = new Date();
 
 const sizingArr = gen.generateSizingJSON();
-const questionArr = gen.generateQuestionJSON();
 
 db.bulk({docs: sizingArr})
   .then((body) => {
     console.log('size added: ', body.length);
-    return db.bulk({docs: questionArr})
+    recursiveSeedQuestions(0,2);
   })
-  .then((body) => {
-    console.log('question added: ', body.length);
-  })
+  
+  const recursiveSeedQuestions = (currentTime, end) => {
+    const questionArr = gen.generateQuestionJSON();
+    db.bulk({docs: questionArr})
+      .then((rows) => {
+        startCountQuestions++;
+        totalCountQuestions+= rows.length;
+        if (currentTime !== end) {
+          recursiveSeedQuestions(currentTime + 1, end);
+        } 
+        if (currentTime === end) {
+          console.log('time to complete seed question', new Date() - startTime);
+          console.log('done seeding questions, added', totalCountQuestions);
+        }
+      })
+  };
