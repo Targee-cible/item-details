9b756638521f19f2b2971af669903c20d31eb643
tjlee527 <tjlee527@gmail.com>
2019-09-15 15:08:49 -0700

    able to recursively call seedingQuestions

--- db/neo4j/indexNeo.js
+++ db/neo4j/indexNeo.js
@@ -3,12 +3,42 @@ const neo4j = require('neo4j-driver').v1;
 const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('tiff', 'pw123'));
 const session = driver.session();
 
+const maxCountDetail = 5; // reassign for when you want seeding to end
+let startCountDetail = 0;
+// need 50 cycles of questions => maxCountQuestions = 49
+const maxCountQuestions = 5;
+let startCountQuestions = 0;
+let totalCountDetail = 0;
+let totalCountQuestions = 0;
+const startTime = new Date();
+
 session
-  .run("LOAD CSV WITH HEADERS FROM \"file:///Users/TiffanyLee/Desktop/HR/SDC/item-details/db/neo4j/neo4Data/size.csv\" AS Line \
-  RETURN Line[0]")
+  .run("LOAD CSV WITH HEADERS FROM \"file:///Users/TiffanyLee/Desktop/HR/SDC/item-details/db/neo4j/neo4Data/sizingNeoCSV.csv\" AS LINE CREATE(n:sizing)")
   .then(() => {
-    console.log('created');
+    console.log('created sizing added:');
+    recursiveSeedQuestions(0, 0, maxCountQuestions);
   })
   .catch((err) => {
     console.log(err);
   });
+
+
+const seedQuestions = (fileCount) => {
+  const path = `LOAD CSV WITH HEADERS FROM \"file:///Users/TiffanyLee/Desktop/HR/SDC/item-details/db/neo4j/neo4Data/detailNeoCSV${fileCount}.csv\" AS LINE CREATE(n:questions)`;
+  return session.run(path);
+}
+
+const recursiveSeedQuestions = (fileNum, currentTime, end) => {
+  seedQuestions(fileNum)
+    .then(() => {
+      startCountQuestions++;
+      totalCountQuestions+= 1000000;
+      if (currentTime !== end) {
+        recursiveSeedQuestions(fileNum + 1, currentTime + 1, end);
+      }
+      if (currentTime === end) {
+        console.log('time to complete seed question', new Date() - startTime);
+        console.log('done seeding questions');
+      }
+    })
+};
