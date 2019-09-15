3847ad559e324c4fe5b12f29b5fedd399da7687a
tjlee527 <tjlee527@gmail.com>
2019-09-15 13:52:01 -0700

    added code for itemId increments for neoCSVGen

--- db/neo4j/neoCSVGen.js
+++ db/neo4j/neoCSVGen.js
@@ -5,9 +5,9 @@ const fakerBlurb = require('../couchDB/fakerBlurbs.js');
 // TO CREATE JSON FOR SIZING
 
 
-const sizingCSV = `type,size,neck,chest,sleeve\nMen - Shirts, XS, 13-13.5, 30-32, 31.5-32 \nMen - Shirts, S, 14-14.5, 34-36, 32.5-33\nMen - Shirts, M, 15-15.5, 38-40, 33.5-34\nMen - Shirts, L, 16-16.5, 42-44, 34.5-35\nMen - Shirts, XL, 17-17.5, 46-48, 35.5-36\nMen - Shirts, XXL, 18-18.5, 50-52, 36.5-37\nMen - Shirts, XXXL, 19-19.5, 54-56, 37.5-38`;
+// const sizingCSV = `type,size,neck,chest,sleeve\nMen - Shirts, XS, 13-13.5, 30-32, 31.5-32 \nMen - Shirts, S, 14-14.5, 34-36, 32.5-33\nMen - Shirts, M, 15-15.5, 38-40, 33.5-34\nMen - Shirts, L, 16-16.5, 42-44, 34.5-35\nMen - Shirts, XL, 17-17.5, 46-48, 35.5-36\nMen - Shirts, XXL, 18-18.5, 50-52, 36.5-37\nMen - Shirts, XXXL, 19-19.5, 54-56, 37.5-38`;
 
-fs.writeFileSync(`./db/neo4j/neo4Data/sizingNeoCSV.csv`, sizingCSV);
+// fs.writeFileSync(`./db/neo4j/neo4Data/sizingNeoCSV.csv`, sizingCSV);
 
 // // TO CREATE JSON FOR QUESTIONS
 // // generate questions data
@@ -70,84 +70,85 @@ fs.writeFileSync(`./db/neo4j/neo4Data/sizingNeoCSV.csv`, sizingCSV);
 // }
 
 
-// // TO CREATE JSON FOR ITEM DETAIL
-// const randomBulletPoints = function () {
-//   const options = Math.floor(Math.random() * Math.floor(5));
-//   const pointsToList = ['null', 'null', 'null', 'null', 'null'];
-//   if (options === 0) {
-//     return pointsToList;
-//   }
-//   let i = 0;
-//   while (i <= options) {
-//     pointsToList[i] = fakerBlurb.sentence[Math.floor(Math.random() * Math.floor(201))];
-//     i += 1;
-//   }
-//   return pointsToList;
-// };
+// TO CREATE JSON FOR ITEM DETAIL
+const randomBulletPoints = function () {
+  const options = Math.floor(Math.random() * Math.floor(5));
+  const pointsToList = ['null', 'null', 'null', 'null', 'null'];
+  if (options === 0) {
+    return pointsToList;
+  }
+  let i = 0;
+  while (i <= options) {
+    pointsToList[i] = fakerBlurb.sentence[Math.floor(Math.random() * Math.floor(201))];
+    i += 1;
+  }
+  return pointsToList;
+};
 
 
 
-//   // loop to add into item detail table, will have to adjust the 100 num
-// const generateDetailJSON = function (currentNum) {
-//   const pointsToList = randomBulletPoints();
-//   const randomNum = Math.floor(Math.random() * Math.floor(200));
+  // loop to add into item detail table, will have to adjust the 100 num
+const generateDetailJSON = function (currentNum, cycle) {
+  const pointsToList = randomBulletPoints();
+  const randomNum = Math.floor(Math.random() * Math.floor(200));
+  
 
-//   // data constants 
-//   const itemId = currentNum;
-//   const point1 = pointsToList[0];
-//   const point2 = pointsToList[1];
-//   const point3 = pointsToList[2];
-//   const point4 = pointsToList[3];
-//   const point5 = pointsToList[4];
-//   const blurb = fakerBlurb.para[randomNum];
-//   const sizing = faker.random.word();
-//   const material = fakerBlurb.commerce[Math.floor(randomNum/2)];
-//   const fit = fakerBlurb.commerce[Math.floor(randomNum/3)];
-//   const length = fakerBlurb.words1[Math.floor(randomNum/2)];
-//   const features = fakerBlurb.commerce[randomNum];
-//   const neckline = fakerBlurb.commerce[Math.floor(randomNum/2)];
-//   const itemStyle = fakerBlurb.commerce[Math.floor(randomNum/3)];
-//   const garmentCuffCutType = fakerBlurb.words1[randomNum];
-//   const garmentSleeveStyle = fakerBlurb.words2[randomNum];
-//   const careAndCleaning = fakerBlurb.words3[Math.floor(randomNum/3)];
-//   const TCIN = faker.random.number();
-//   const UPC = faker.random.number();
-//   const DPCI = faker.random.number();
-//   const origin = fakerBlurb.words3[randomNum];
-//   const recycledPolyester = faker.random.boolean();
-//   const fastShipping = faker.random.boolean();
-//   const estimatedShipDimensions = fakerBlurb.sentence[randomNum];
-//   const estimatedShipWeight = fakerBlurb.words1[Math.floor(randomNum/3)];
-//   const type = 'Men - Shirts'
+  // data constants 
+  const itemId = (cycle * 1000000) + currentNum;
+  const point1 = pointsToList[0];
+  const point2 = pointsToList[1];
+  const point3 = pointsToList[2];
+  const point4 = pointsToList[3];
+  const point5 = pointsToList[4];
+  const blurb = fakerBlurb.para[randomNum];
+  const sizing = faker.random.word();
+  const material = fakerBlurb.commerce[Math.floor(randomNum/2)];
+  const fit = fakerBlurb.commerce[Math.floor(randomNum/3)];
+  const length = fakerBlurb.words1[Math.floor(randomNum/2)];
+  const features = fakerBlurb.commerce[randomNum];
+  const neckline = fakerBlurb.commerce[Math.floor(randomNum/2)];
+  const itemStyle = fakerBlurb.commerce[Math.floor(randomNum/3)];
+  const garmentCuffCutType = fakerBlurb.words1[randomNum];
+  const garmentSleeveStyle = fakerBlurb.words2[randomNum];
+  const careAndCleaning = fakerBlurb.words3[Math.floor(randomNum/3)];
+  const TCIN = faker.random.number();
+  const UPC = faker.random.number();
+  const DPCI = faker.random.number();
+  const origin = fakerBlurb.words3[randomNum];
+  const recycledPolyester = faker.random.boolean();
+  const fastShipping = faker.random.boolean();
+  const estimatedShipDimensions = fakerBlurb.sentence[randomNum];
+  const estimatedShipWeight = fakerBlurb.words1[Math.floor(randomNum/3)];
+  const type = 'Men - Shirts'
  
-//   return `${itemId},${point1},${point2},${point3},${point4},${point5},${blurb},${sizing},${material},${fit},${length},${features},${neckline},${itemStyle},${garmentCuffCutType},${garmentSleeveStyle},${careAndCleaning},${TCIN},${UPC},${DPCI},${origin},${recycledPolyester},${fastShipping},${estimatedShipDimensions},${estimatedShipWeight},${type}`;
-// };
+  return `${itemId},${point1},${point2},${point3},${point4},${point5},${blurb},${sizing},${material},${fit},${length},${features},${neckline},${itemStyle},${garmentCuffCutType},${garmentSleeveStyle},${careAndCleaning},${TCIN},${UPC},${DPCI},${origin},${recycledPolyester},${fastShipping},${estimatedShipDimensions},${estimatedShipWeight},${type}`;
+};
 
 
 
-// const generateAllDetail = function(path, length) {
-//   return new Promise((resolve, reject) => {
-//     const output = fs.createWriteStream(path, {encoding: 'utf8'});
-//     output.write('itemId, point1, point2, point3, point4, point5, blurb, sizing, material, fit, length, features, neckline, itemStyle, garmentCuffCutType,garmentSleeveStyle,careAndCleaning,TCIN,UPC,DPCI,origin,recycledPolyester,fastShipping,estimatedShipDimensions,estimatedShipWeight,type');
+const generateAllDetail = function(path, cycle) {
+  return new Promise((resolve, reject) => {
+    const output = fs.createWriteStream(path, {encoding: 'utf8'});
+    output.write('itemId, point1, point2, point3, point4, point5, blurb, sizing, material, fit, length, features, neckline, itemStyle, garmentCuffCutType,garmentSleeveStyle,careAndCleaning,TCIN,UPC,DPCI,origin,recycledPolyester,fastShipping,estimatedShipDimensions,estimatedShipWeight,type');
 
-//     (async() => {
-//       for (var i = 0; i < length; i++) {
-//         const obj = generateDetailJSON(i);
+    (async() => {
+      for (var i = 0; i < 1000000; i++) {
+        const obj = generateDetailJSON(i, cycle, 1000000);
 
-//         if(!output.write(obj)) {
-//           await new Promise(resolve => output.once('drain', resolve));
-//         }
-//         if (i !== length) {
-//           output.write('\n');
-//         }
-//       }
-//       output.end();
-//     })();
-//     output.on('finish', () => resolve('detal generated'));
-//   })
-// }
+        if(!output.write(obj)) {
+          await new Promise(resolve => output.once('drain', resolve));
+        }
+        if (i !== 1000000) {
+          output.write('\n');
+        }
+      }
+      output.end();
+    })();
+    output.on('finish', () => resolve('detal generated'));
+  })
+}
 
 
-// for (var i = 0; i < 10; i++) {
-//   generateAllDetail(`./db/neo4j/neo4Data/detailNeoCSV${i}.csv`, 1000000);
-// }
+for (var i = 0; i < 10; i++) {
+  generateAllDetail(`./db/neo4j/neo4Data/detailNeoCSV${i}.csv`, i);
+}
