0af63a750141b1b4ac53400cebd06fa3ad1aa960
tjlee527 <tjlee527@gmail.com>
2019-09-16 11:15:56 -0700

    created seedDetail recusursive function

--- db/neo4j/indexNeo.js
+++ db/neo4j/indexNeo.js
@@ -3,42 +3,63 @@ const neo4j = require('neo4j-driver').v1;
 const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('tiff', 'pw123'));
 const session = driver.session();
 
-const maxCountDetail = 5; // reassign for when you want seeding to end
-let startCountDetail = 0;
-// need 50 cycles of questions => maxCountQuestions = 49
-const maxCountQuestions = 5;
-let startCountQuestions = 0;
+const maxDetailCycle = 2;
+const maxQuestionCycle = 4;
 let totalCountDetail = 0;
 let totalCountQuestions = 0;
 const startTime = new Date();
 
-session
-  .run("LOAD CSV WITH HEADERS FROM \"file:///Users/TiffanyLee/Desktop/HR/SDC/item-details/db/neo4j/neo4Data/sizingNeoCSV.csv\" AS LINE CREATE(n:sizing)")
-  .then(() => {
-    console.log('created sizing added:');
-    recursiveSeedQuestions(0, 0, maxCountQuestions);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// session
+//   .run("LOAD CSV WITH HEADERS FROM \"file:///Users/TiffanyLee/Desktop/HR/SDC/item-details/db/neo4j/neo4Data/sizingNeoCSV.csv\" AS LINE CREATE(n:sizing)")
+//   .then(() => {
+//     console.log('created sizing added:');
+//     recursiveSeedQuestions(0, 0, maxCountQuestions);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 
-const seedQuestions = (fileCount) => {
-  const path = `LOAD CSV WITH HEADERS FROM \"file:///Users/TiffanyLee/Desktop/HR/SDC/item-details/db/neo4j/neo4Data/detailNeoCSV${fileCount}.csv\" AS LINE CREATE(n:questions)`;
+// const seedQuestions = (fileCount) => {
+//   const path = `LOAD CSV WITH HEADERS FROM \"file:///Users/TiffanyLee/Desktop/HR/SDC/item-details/db/neo4j/neo4Data/QuesNeoCSV${fileCount}.csv\" AS LINE CREATE(n:questions)`;
+//   return session.run(path);
+// }
+
+// const recursiveSeedQuestions = (fileNum, currentTime, end) => {
+//   seedQuestions(fileNum)
+//     .then(() => {
+//       totalCountQuestions+= 1000000;
+//       if (currentTime !== end) {
+//         recursiveSeedQuestions(fileNum + 1, currentTime + 1, end);
+//       }
+//       if (currentTime === end) {
+//         console.log('time to complete seed question', new Date() - startTime);
+//         console.log('done seeding questions');
+//         recursiveSeedDetail(0, 0, maxQuestions);
+//       }
+//     })
+// };
+
+const seedDetail = (fileCount) => {
+  const path = `LOAD CSV WITH HEADERS FROM \"file:///Users/TiffanyLee/Desktop/HR/SDC/item-details/db/neo4j/neo4Data/detailNeoCSV${fileCount}.csv\" AS LINE CREATE(n:detail)`;
   return session.run(path);
 }
 
-const recursiveSeedQuestions = (fileNum, currentTime, end) => {
-  seedQuestions(fileNum)
+const recursiveSeedDetail = (fileNum, currentTime, end) => {
+  seedDetail(fileNum)
     .then(() => {
-      startCountQuestions++;
-      totalCountQuestions+= 1000000;
+      totalCountDetail+= 1000000;
       if (currentTime !== end) {
-        recursiveSeedQuestions(fileNum + 1, currentTime + 1, end);
+        recursiveSeedDetail(fileNum + 1, currentTime + 1, end);
       }
       if (currentTime === end) {
-        console.log('time to complete seed question', new Date() - startTime);
-        console.log('done seeding questions');
+        driver.close();
+        console.log('time to complete all seeding', new Date() - startTime);
+        console.log('done seeding detail');
+        console.log('total questions added', totalCountQuestions);
+        console.log('total detail added', totalCountDetail);
       }
     })
 };
+
+recursiveSeedDetail(0, 0, maxDetailCycle);
